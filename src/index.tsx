a307ea5fffa56ab51f492edd601f20a324c4a688
Rongjian Zhang <pd4d10@gmail.com>
2018-10-29 13:10:33 +0800

    feat: first implementation of QRCode scanner

---
+++ src/index.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import jsQR from 'jsqr'
+
+type ScannerProps = {
+  /**
+   * Scan interval
+   */
+  interval?: number
+  /**
+   * Scan type
+   */
+  type: 'qrcode' | 'barcode'
+  /**
+   * Scan result callback
+   */
+  onScan: (result: string | null) => void
+}
+
+export default class Scanner extends React.Component<ScannerProps> {
+  static defaultProps: Partial<ScannerProps> = {
+    interval: 500,
+  }
+
+  videoRef = React.createRef<HTMLVideoElement>()
+  canvasRef = React.createRef<HTMLCanvasElement>()
+
+  setup = async () => {
+    const devices = await navigator.mediaDevices.enumerateDevices()
+    const videoInputDevices = devices.filter(
+      device => device.kind === 'videoinput',
+    )
+    if (videoInputDevices.length === 0) {
+      return
+    }
+
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: videoInputDevices[0],
+    })
+    const $video = this.videoRef.current!
+    $video.srcObject = stream
+  }
+
+  scan = () => {
+    const $canvas = this.canvasRef.current!
+    const context = $canvas.getContext('2d')!
+    context.drawImage(this.videoRef.current!, 0, 0)
+    const imageData = context.getImageData(0, 0, $canvas.width, $canvas.height)
+    const result = jsQR(imageData.data, $canvas.width, $canvas.height)
+
+    this.props.onScan(result ? result.data : null)
+    setTimeout(() => {
+      this.scan()
+    }, this.props.interval!)
+  }
+
+  componentDidMount() {
+    this.setup()
+    this.scan()
+  }
+
+  render() {
+    return (
+      <>
+        <video ref={this.videoRef} />
+        <canvas style={{ display: 'none' }} ref={this.canvasRef} />
+      </>
+    )
+  }
+}
